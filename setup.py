bc66ef232b2f7ac8967d8449acd7853daa5b2a04
Armand <adpl33@gmail.com>
2021-01-25 12:02:33 +0100

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ks_nlp", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Armand du Parc Locmaria",
     author_email="adpl33@gmail.com",
     description="NLP Hackathon toolbox",
